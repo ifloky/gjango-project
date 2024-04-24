1e512d8a1aedf662e4cfe45dd0bd98f0ce32adc8
iflocky <Petrenko1702@gmail.com>
2024-04-24 17:11:59 +0300

    feat: add base and articles

--- myWebSite/main/views.py
+++ myWebSite/main/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request): 
-  return render(request, 'main/index.html')
+  data = {
+    'title': 'Главная страница',
+    'values': ['Some', 'Hello', '123'],
+    'obj': {
+      'car': 'Volvo',
+      'age': 29,
+      'hobby': 'work'
+    }
+  }
+  return render(request, 'main/index.html', data)
 
 def about(request): 
   return render(request, 'main/about.html')
--- myWebSite/myWebSite/settings.py
+++ myWebSite/myWebSite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'news'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+  BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
