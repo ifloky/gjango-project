6f6eb372bba173c1cdd4b49228e4e0a8e3e438af
iflocky <Petrenko1702@gmail.com>
2024-04-26 15:23:34 +0300

    feat: add update articles

--- myWebSite/news/urls.py
+++ myWebSite/news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
-  path('', views.news_home, name="news_home"),
+    path('', views.news_home, name="news_home"),
+    path('create/', views.create, name="create"),
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail'),
+    path('<int:pk>/update/', views.NewsUpdateView.as_view(), name='news-update'),  # Ensure the correct URL pattern
 ]
--- myWebSite/news/views.py
+++ myWebSite/news/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm 
+from django.views.generic import DetailView, UpdateView
 
 def news_home(request):
   news = Articles.objects.order_by('date')
   return render(request, 'news/news_home.html', {'news':news, 'title': 'Новости'})
+
+class NewsDetailView(DetailView):
+  model = Articles
+  template_name = 'news/details_view.html'
+  context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+  model = Articles
+  template_name = 'news/create.html'
+  form_class = ArticlesForm
+
+def create(request): 
+  error = ''
+
+  if request.method == 'POST':
+    form = ArticlesForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('news')
+    else: 
+      error = 'Форма была неверной'
+
+  form = ArticlesForm()
+
+  data = {
+    'form': form,
+    'error': error
+  }
+
+  return render(request, 'news/create.html', {'title': "Добавление статьи", 'data':data})
